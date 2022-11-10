dd4952c93e627e4e9f490476f526cd4cd5de41c2
Shefaa Sumait <83834378+Shefaabi@users.noreply.github.com>
2022-11-10 10:02:02 +0200

    Update Client.h

--- Client.h
+++ Client.h
@@ -17,21 +17,17 @@ private:
     string address;
     string phoneNumber;
     BankAccount *account;
-    BankApplication *app;
 
 public:
     // ========== constructors ==========
     Client(){};
     Client(string &name, string &address, string &phoneNumber, BankApplication* a);
-
     Client (string name, string address, int phoneNumber , BankApplication* a);
+    
     void createAccount(char type, double balance);
-
     void createAccount(int &type, double &balance);
-    void updateBalance(double nBalance);
 
-
-    BankAccount get_account();
+    BankAccount &get_account();
     friend ostream &operator<<(ostream &out, Client c);
 };
 #endif
