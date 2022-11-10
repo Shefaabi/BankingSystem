33bd286f63b05ff94fa002c4aabd61bc52467279
Shefaa Sumait <83834378+Shefaabi@users.noreply.github.com>
2022-11-10 09:56:02 +0200

    Update BankAccount.cpp

--- BankAccount.cpp
+++ BankAccount.cpp
@@ -1,28 +1,27 @@
 #include "BankAccount.h"
 
-int BankAccount :: id = 0;
+int BankAccount ::id = 0;
 
-BankAccount::BankAccount(double balance, BankApplication* a , int type )
+BankAccount::BankAccount(double balance, int type)
 {
     this->balance = balance;
-//    this->owner = &owner;
-    this->app = a;
     this->type = type;
+
     set_accountID();
     cout << "An account was created with ID: " << accountID
          << " and Starting Balance " << balance << "L.E.\n";
 }
 
 // ========== set & get ==========
-void BankAccount :: set_accountID()
+void BankAccount ::set_accountID()
 {
     id++;
     int dif;
     string temp;
     dif = 3 - to_string(id).size();
-    for(int i = 0; i < dif ; i++)
+    for (int i = 0; i < dif; i++)
     {
-         temp += '0';
+        temp += '0';
     }
     temp += to_string(id);
 
@@ -34,15 +33,11 @@ void BankAccount::set_balance(double balance)
     this->balance = balance;
 }
 
-// void set_client()
-
 // ========== operations ==========
 double BankAccount::withdraw(double amount)
 {
     bool is_valid = false;
     double newBalance = balance - amount;
-
-
 
     while (!is_valid)
     {
@@ -50,10 +45,7 @@ double BankAccount::withdraw(double amount)
         {
             set_balance(balance - amount);
             is_valid = true;
-             receipt();
-             app->balanceEditing(newBalance,accountID);
-             return get_balance();
-
+            receipt();
         }
         else
         {
@@ -61,25 +53,24 @@ double BankAccount::withdraw(double amount)
                     "Please enter valid amount\n";
             cin >> amount;
         }
-
     }
-
     return get_balance();
 }
 
-double BankAccount :: deposit(double amount)
+double BankAccount ::deposit(double amount)
 {
-    set_balance(balance + amount); // need to validate amount
-    app->balanceEditing(balance, accountID);
-     receipt();
-     return get_balance();
+    set_balance(balance + amount);
+    receipt();
+    return get_balance();
 }
 
 void BankAccount::receipt()
 {
+    cout << "-------------------\n";
     cout << "Thank you.\n";
-     cout << "Account ID: " << get_accountID() << '\n';
-     cout << "New Balance: " << get_balance() << endl;
+    cout << "Account ID: " << get_accountID() << '\n';
+    cout << "New Balance: " << get_balance() << endl;
+    cout << "-------------------\n";
 }
 
 SavingBankAccount::SavingBankAccount(double balance, int type)
@@ -116,4 +107,37 @@ double SavingBankAccount::withdraw(double amount)
             cin >> amount;
         }
     }
+}
+
+double SavingBankAccount ::deposit(double amount)
+{
+    int select;
+    while (true)
+    {
+        if (amount >= 100)
+        {
+            set_balance(balance + amount);
+            receipt();
+            break;
+        }
+
+        else
+        {
+            cout << "Sorry, you have to deposit 100 L.E. or more\n";
+            cout << "Enter (1) to change the amount\nEnter (2) to exit\n";
+            cin >> select;
+
+            if (select == 1)
+            {
+                cout << "Enter the amount that you want to deposit\n";
+                cin >> amount;
+            }
+            else if (select == 2)
+            {
+                break;
+            }
+        }
+    }
+
+    return get_balance();
 }
