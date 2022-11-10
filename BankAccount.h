82bfea3d9e5c6a61c27322ab9adf137d83c57045
Shefaa Sumait <83834378+Shefaabi@users.noreply.github.com>
2022-11-10 09:56:39 +0200

    Update BankAccount.h

--- BankAccount.h
+++ BankAccount.h
@@ -16,76 +16,24 @@ class BankAccount
 protected:
     string accountID;
     double balance;
-    static int id ;
+    static int id;
     int type;
-    BankApplication *app;
-    Client *owner;
+    // BankApplication *app;
+    // Client *owner;
 
 public:
     // ========== constructors ==========
     BankAccount() { balance = 0.0; }
 
-    BankAccount(double balance , BankApplication * a , int type);
+    BankAccount(double balance, int type);
     void set_accountID();
     void set_balance(double balance);
     string get_accountID() { return accountID; }
     double get_balance() { return balance; }
-    int get_type(){ return type; }
-    double withdraw(double amount);
-    double deposit(double amount);
+    int get_type() { return type; }
+    virtual double withdraw(double amount);
+    virtual double deposit(double amount);
     void receipt();
-    // BankAccount(double balance, Client *owner)
-    // {
-    //     this->balance = balance;
-    //     this->owner = owner;
-    //     set_accountID();
-    //     cout << "An account was created with ID" << accountID
-    //          << " and Starting Balance " << balance << "L.E.";
-    // }
-
-    // // ========== set & get ==========
-
-
-//     void set_balance(double balance)
-//     {
-//         this->balance = balance;
-//     }
-
-    // // void set_client()
-
-    // string get_accountID() { return accountID; }
-    // double get_balance() { return balance; }
-
-    // // ========== operations ==========
-    // int withdraw(double amount)
-    // {
-    //     // need a loop
-    //     if (amount <= balance)
-    //     {
-    //         set_balance(balance - amount);
-    //         // receipt();
-    //         return get_balance();
-    //     }
-
-    //     else
-    //     {
-    //         cout << "Sorry. This is more than what you can withdraw.\n";
-    //     }
-    // }
-
-    // int deposite(double amount)
-    // {
-    //     set_balance(balance + amount); // need to validate amount
-    //                                    // receipt();
-    //     return get_balance();
-    // }
-
-    // void receipt()
-    // {
-    //     cout << "Thank you.\n";
-    //     cout << "Account ID: " << get_accountID();
-    //     cout << "New Balance: " << get_balance() << endl;
-    // }
 };
 
 class SavingBankAccount : public BankAccount
@@ -97,5 +45,6 @@ public:
     // ========== constructors ==========
     SavingBankAccount(double balance, int type);
     double withdraw(double amount);
+    double deposit(double amount);
 };
 #endif
