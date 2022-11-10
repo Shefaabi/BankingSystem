89f78147d879828971623d061050394457447004
Shefaa Sumait <83834378+Shefaabi@users.noreply.github.com>
2022-11-10 10:02:19 +0200

    Update Client.h

--- Client.h
+++ Client.h
@@ -4,11 +4,11 @@
 #include <iostream>
 #include "BankApp.h"
 
-
 using namespace std;
 
 class BankAccount;
 class BankApplication;
+class SavingBankAccount;
 
 class Client
 {
@@ -21,13 +21,16 @@ private:
 public:
     // ========== constructors ==========
     Client(){};
-    Client(string &name, string &address, string &phoneNumber, BankApplication* a);
-    Client (string name, string address, int phoneNumber , BankApplication* a);
-    
+    Client(string &name, string &address, string &phoneNumber);
+    Client(string name, string address, int phoneNumber);
+
     void createAccount(char type, double balance);
     void createAccount(int &type, double &balance);
 
     BankAccount &get_account();
+    SavingBankAccount &get();
+
     friend ostream &operator<<(ostream &out, Client c);
+    // ~Client() { delete account; }
 };
 #endif
